a423ceec212aea7aa3f6b9219e822eebdd9909e3
fengyurong <fengyurong@MacBook-Pro-5.local>
2021-02-23 10:53:45 +0800

    init code

---
+++ start/src/test/java/com/fengyr/springLearn/SpringLearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fengyr.springLearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
